3c834dc39061a269c2f0e4d232bf58a48e635e26
A. Dvorak <dvorak88@mail.ru>
2021-01-31 12:50:08 +0400

    feat: signal generator form validation

--- js/signalgenerator.js
+++ js/signalgenerator.js
@@ -1,5 +1,27 @@
 SignalGenerator = {
 
+  VALIDATION_MSG: {
+    number: 'Must be a number',
+    greaterThanZero: 'Must be greater than zero'
+  },
+
+  FORM_VALIDATION: {
+    length: (values) => {
+      if (values.length <= 0) return SignalGenerator.VALIDATION_MSG.greaterThanZero
+    },
+    step: (values) => {
+      if (values.step <= 0) return SignalGenerator.VALIDATION_MSG.greaterThanZero
+      if (!SignalGenerator.FORM_VALIDATION.length(values) && values.length / values.step < 10)
+        return 'Must be at least 10 times less than length'
+    },
+    period: (values) => {
+      if (values.period <= 0) return SignalGenerator.VALIDATION_MSG.greaterThanZero
+    },
+    amplitude: (values) => {
+      if (values.amplitude <= 0) return SignalGenerator.VALIDATION_MSG.greaterThanZero
+    },
+  },
+
   SIGNAL_FORMS: {
     sine: ({x, period, amplitude, offset}) => {
       return offset + amplitude * Math.sin(x * 2 * Math.PI / period)
@@ -23,43 +45,94 @@ SignalGenerator = {
 
   selectElements() {
     this.ui = {}
-    this.ui.beginInp = $('#SignalGeneratorBegin')
-    this.ui.lengthInp = $('#SignalGeneratorLength')
-    this.ui.stepInp = $('#SignalGeneratorStep')
-    this.ui.periodInp = $('#SignalGeneratorPeriod')
-    this.ui.amplitudeInp = $('#SignalGeneratorAmplitude')
-    this.ui.offsetInp = $('#SignalGeneratorOffset')
-    this.ui.formSel = $('#SignalGeneratorForm')
+    this.ui.signalParamForm = {}
+    this.ui.signalParamForm.begin = $('#SignalGeneratorBegin')
+    this.ui.signalParamForm.beginValid= $('#SignalGeneratorBeginValid')
+    this.ui.signalParamForm.length = $('#SignalGeneratorLength')
+    this.ui.signalParamForm.lengthValid = $('#SignalGeneratorLengthValid')
+    this.ui.signalParamForm.step = $('#SignalGeneratorStep')
+    this.ui.signalParamForm.stepValid = $('#SignalGeneratorStepValid')
+    this.ui.signalParamForm.period = $('#SignalGeneratorPeriod')
+    this.ui.signalParamForm.periodValid = $('#SignalGeneratorPeriodValid')
+    this.ui.signalParamForm.amplitude = $('#SignalGeneratorAmplitude')
+    this.ui.signalParamForm.amplitudeValid = $('#SignalGeneratorAmplitudeValid')
+    this.ui.signalParamForm.offset = $('#SignalGeneratorOffset')
+    this.ui.signalParamForm.offsetValid = $('#SignalGeneratorOffsetValid')
+    this.ui.signalParamForm.form = $('#SignalGeneratorForm')
     this.ui.saveBtn = $('#SignalGeneratorSave')
     this.ui.fileInp = $('#SignalGeneratorFile')
   },
 
   fillFormSelector() {
     for (let form in this.SIGNAL_FORMS) {
-      this.ui.formSel.append(`<option>${form}</option>`)
+      this.ui.signalParamForm.form.append(`<option>${form}</option>`)
     }
   },
 
   initEvents() {
     this.ui.saveBtn.on('click', () => {
-      this.generateAndOpenSaver(this.getFormValues())
+      let values = this.getSignalParamValues()
+      if (this.validateSignalParamValues(values)) {
+        this.generateAndOpenSaver(values)
+      }
     })
     this.ui.fileInp.on('change', () => {
       this.importFromFile().then()
     })
   },
 
-  getFormValues() {
-    let ui = this.ui
+  getSignalParamValues() {
+    let form = this.ui.signalParamForm
     return {
-      begin: parseFloat(ui.beginInp.val()),
-      length: parseFloat(ui.lengthInp.val()),
-      step: parseFloat(ui.stepInp.val()),
-      period: parseFloat(ui.periodInp.val()),
-      amplitude: parseFloat(ui.amplitudeInp.val()),
-      offset: parseFloat(ui.offsetInp.val()),
-      form: ui.formSel.val()
+      begin: parseFloat(form.begin.val()),
+      length: parseFloat(form.length.val()),
+      step: parseFloat(form.step.val()),
+      period: parseFloat(form.period.val()),
+      amplitude: parseFloat(form.amplitude.val()),
+      offset: parseFloat(form.offset.val()),
+      form: form.form.val()
     }
+  },
+
+  validateSignalParamValues(values) {
+    let validated = true
+    for (let fieldName in values) {
+      if (fieldName !== 'form') {
+        let validationMsg = this.getFieldValidationMsg(fieldName, values)
+        if (validationMsg) {
+          this.markInvalid(fieldName, validationMsg)
+          validated = false
+        } else {
+          this.removeInvalidIndication(fieldName)
+        }
+      }
+    }
+    return validated
+  },
+
+  getFieldValidationMsg(fieldName, values) {
+    let value = values[fieldName]
+    if (isNaN(value)) {
+      return this.VALIDATION_MSG.number
+    } else {
+      let customFieldValidation = this.FORM_VALIDATION[fieldName]
+      return customFieldValidation ? customFieldValidation(values) : ''
+    }
+  },
+
+  markInvalid(fieldName, msg) {
+    let form = this.ui.signalParamForm
+    form[fieldName].addClass('is-invalid')
+    form[fieldName + 'Valid'].html(msg)
+  },
+
+  removeInvalidIndication(fieldName) {
+    let form = this.ui.signalParamForm
+    form[fieldName].removeClass('is-invalid')
+  },
+
+  getFileName() {
+    return this.ui.fileInp.val().replace(/C:\\fakepath\\/, '')
   },
 
   generateAndOpenSaver({begin, length, step, period, amplitude, offset, form}) {
@@ -79,7 +152,7 @@ SignalGenerator = {
     let data = await FileManager.readFile(this.ui.fileInp)
     this.sendToCable({
       name: `Imported signal`,
-      description: `Imported from file ${this.ui.fileInp.val()}`,
+      description: `Imported from file ${this.getFileName()}`,
       data
     })
   },
